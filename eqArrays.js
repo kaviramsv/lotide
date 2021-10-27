e2c9fa6428f1328cccac737a53dc75331f127e64
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-27 01:08:07 +0000

    eqArrays modified for length

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,22 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-const eqArrays= function(arr1,arr2){
-
-  for(let i=0;i<=arr1.length-1;i++){
-    for(let j=0;j<=arr2.length-1;j++){
-      // console.log(arr1[i],arr2[i]);
-       if(arr1[i]!==arr2[i]){
-         return false;
-       }
-     
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false
+  } else {
+    for (let i = 0; i <= arr1.length - 1; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-}
+}//else
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
 
