35a315b6e25d508c41604a99740419a6dabe400d
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-27 18:33:08 +0000

    add countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+const countOnly = function(allItems, itemsToCount) {
+
+const new_obj={};
+
+for(const key in itemsToCount){
+
+  if(itemsToCount[key]===true){
+    let ct=0;
+     for(let j=0;j<allItems.length;j++){
+        if (allItems[j]==key){
+          ct++;
+          
+        }
+     }console.log(ct);
+     if(ct>0){
+     new_obj[key]=ct;
+     }
+  }
+
+}
+console.log(new_obj);
+return new_obj;
+
+}
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
