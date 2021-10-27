f2eee5126658da7e78c228b123a3389d072d0950
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-27 19:10:36 +0000

    add assert statement

--- countLetters.js
+++ countLetters.js
@@ -36,3 +36,12 @@ const assertEqual = function(actual, expected) {
  }
  
  console.log(countLetters("lighthouse in the house"));
+
+
+//  const ans={ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
+ 
+ const result1 =countLetters("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 4);
