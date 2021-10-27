dc1f8c926aad4349ace46ab40e3026877dc04b61
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-27 11:18:56 +0000

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays= function(arr1,arr2){
+
+  for(let i=0;i<=arr1.length-1;i++){
+   // console.log(arr1[i],arr2[i]);
+       if(arr1[i]!==arr2[i]){
+         return false;
+       }   
+      }
+  return true;
+}
+
+const assertArraysEqual=function (arr1,arr2){
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function (arr){
+  let new_arr=[];
+  for(let item of arr){
+
+    if (Array.isArray(item)){
+      for(let i of item)
+      {
+        new_arr.push(i);
+      }
+    }else{
+      new_arr.push(item);
+    }
+  }
+  // console.log(new_arr);
+  return new_arr;
+}
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6])
