050025c1da5704e5075bc88547e0e67559b9192c
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-28 20:43:55 +0000

    add map fn

---
+++ map.js
@@ -0,0 +1,41 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i <= arr1.length - 1; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual=function (arr1,arr2){
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  // temporary code:
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    }
+  //each item passed to the callback
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1,[ 'g', 'c', 't', 'm', 't' ]);
