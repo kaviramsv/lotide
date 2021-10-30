e0c5574be3ecf2392ed0abda79af07b3bc20c128
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-30 20:30:35 +0000

    eqArrays add recursion

--- eqArrays.js
+++ eqArrays.js
@@ -11,14 +11,17 @@ const eqArrays = function (arr1, arr2) {
   } else {
     for (let i = 0; i <= arr1.length - 1; i++) {
       if (arr1[i] !== arr2[i]) {
+        if(Array.isArray(arr1[i])&&Array.isArray(arr2[i])){//if is array true
+          return eqArrays(arr1[i],arr2[i])
+        }//nested_array
         return false;
       }
     }
     return true;
   }
-}//else
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+}
+console.log(eqArrays([1, [2, 3]], [1,[ 2, 3]])) // => true
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
